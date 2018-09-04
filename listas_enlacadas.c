3c8fbba1faea14d22f00551ed7d5d21fbb9c2265
Jonas Felipe <jonasflp@outlook.com>
2018-09-04 20:00:19 -0300

    Add files via upload

---
+++ listas_enlacadas.c
@@ -0,0 +1,22 @@
+//listas enlaçadas
+//typedef int vetor[10];
+//e no main:
+//vetor i;
+
+struct student{
+	int r_no;
+	char name[20];
+	char course[20];
+	float fees;	
+}stud1 = {01,"jonas","CC",4500};
+
+typedef struct{
+	int dd;
+	int mm;
+	int yy;
+}DATE;
+#include<stdio.h>
+int main(){
+	//struct student stud1= {01,"jonas","CC",4500};
+	return 0;
+}
